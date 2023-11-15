9ac6a0a6fcd84c9ad81339d39780de4146b9550e
Rhythm Shandlya <rhythm@filterpixel.com>
2023-11-15 10:51:57 +0530

    testing

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,18 @@
 import json
-import requests
-import os
 
 def lambda_handler(event, context):
-    issue_url = event['issue']['html_url']
-    
-    # Create Slack payload
-    payload = {'text': f'Issue Created: {issue_url}'}
-    
-    # Send payload to Slack
-    slack_url = os.environ['SLACK_URL']
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(slack_url, data=json.dumps(payload), headers=headers)
-    
-    # Log the response (optional)
-    print(response.text)
+    # Assuming the payload is in the 'body' of the input event
+    try:
+        payload = json.loads(event['body'])
+    except json.JSONDecodeError as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f'Error decoding JSON: {str(e)}')
+        }
+
+    # You can perform any processing on the payload if needed
     
     return {
-        'statusCode': response.status_code,
-        'body': json.dumps('Slack notification sent!')
+        'statusCode': 200,
+        'body': json.dumps(payload)
     }
